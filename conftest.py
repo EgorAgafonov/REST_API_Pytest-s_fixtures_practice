d74a8de76d68610638b363303fc3a29a66bc5d13
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-10-05 17:32:48 +0300

    File updated

--- conftest.py
+++ conftest.py
@@ -4,38 +4,40 @@ from datetime import *
 import json
 from settings import *
 
-# @pytest.fixture()
-# def get_api_key(base_url="https://petfriends.skillfactory.ru/", email=valid_email, password=valid_password) -> json:
-#     """"""
-#
-#     headers = {'email': email, 'password': password}
-#     res = requests.get(base_url + 'api/key', headers=headers)
-#     status = res.status_code
-#     result = ""
-#     try:
-#         result = res.json()
-#     except json.decoder.JSONDecodeError:
-#         result = res.text
-#
-#     assert status == 200
-#     assert 'key' in result
-#
-#     return result['key']
-#
-#
-# @pytest.fixture(autouse=True)
-# def duration_of_test():
-#     start_time = datetime.now()
-#     print(f'\n\nНачало выполнения теста:{start_time} сек.')
-#     yield
-#     end_time = datetime.now()
-#     print(f'\nОкончание выполнения теста:{end_time} сек.')
-#     print(f"\nИТОГО: продолжительность теста: {end_time - start_time} сек.")
+
+@pytest.fixture()
+def get_api_key(base_url="https://petfriends.skillfactory.ru/", email=valid_email, password=valid_password) -> json:
+    """"""
+
+    headers = {'email': email, 'password': password}
+    res = requests.get(base_url + 'api/key', headers=headers)
+    status = res.status_code
+    result = ""
+    try:
+        result = res.json()
+    except json.decoder.JSONDecodeError:
+        result = res.text
+
+    assert status == 200
+    assert 'key' in result
+
+    return result['key']
 
 
-filename = 'tests/random_text.txt'
-with open(filename, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    words = ' '.join(lines).split()
-print(words)
+@pytest.fixture(scope='class', autouse=True)
+def duration_of_test():
+    start_time = datetime.now()
+    print(f'\n\nНачало выполнения теста:{start_time} сек.')
+    yield
+    end_time = datetime.now()
+    print(f'\nОкончание выполнения теста:{end_time} сек.')
+    print(f"\nИТОГО: продолжительность теста: {end_time - start_time} сек.")
+
+
+@pytest.fixture(scope='class', autouse=True)
+def greeting():
+    print('\nТестирование в классе начинается.')
+    yield
+    print(f'\nТестирование в классе окончено.')
+
 
